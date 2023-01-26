c3f3e8c14f4ec62a888bcdff99bdf0c44c915518
Mark Coats <alexco1234@gmail.com>
2023-01-25 22:45:57 -0600

    I struggled with this :(

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const m = document.getElementById('main');
+m.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'MARK is the champion'
